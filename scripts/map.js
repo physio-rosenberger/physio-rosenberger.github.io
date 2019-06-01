38b16bd3e357898f915ae601a4c9512351e02954
rosenbeh <harald.rosenberger@avl.com>
2019-06-01 18:24:20 +0200

    V9: "Leistungen" und "Kontakt", navigation

---
+++ scripts/map.js
@@ -0,0 +1,24 @@
+﻿function createMap() {
+  // https://www.latlong.net/ : Caesargasse 4 Weiz
+  //var myOffice = new google.maps.LatLng(47.218252, 15.628486);
+  showMap(47.218252, 15.628486);
+};
+
+function showMap(lat, lng) {
+  map = new OpenLayers.Map("map");
+  var mapnik = new OpenLayers.Layer.OSM();
+  map.addLayer(mapnik);
+  var lonLat = new OpenLayers.LonLat(lng, lat)
+    .transform(
+      new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+      map.getProjectionObject() // to Spherical Mercator Projection
+  );
+
+  var zoom = 17;
+  map.setCenter(lonLat, zoom);
+  var markers = new OpenLayers.Layer.Markers("Markers");
+  map.addLayer(markers);
+  markers.addMarker(new OpenLayers.Marker(lonLat));
+}
+
+
---
+++ scripts/navigation.js
@@ -0,0 +1,19 @@
+﻿////
+// Replace the placeholder for the navigation element by the imported 'nav-main' element.
+// The placeholder needs to have class "nav-include".
+//
+function includeNavigation() {
+  var placeHolder = document.querySelector('.nav-include');
+  if (!placeHolder)
+    return;
+  var navImport = document.querySelector('link[rel="import"]')
+  if (!navImport || ! navImport.import)
+    return;
+  var navElement = navImport.import.querySelector('.nav-main')
+  if (!navElement)
+    return;
+  var parent = placeHolder.parentNode;
+  var navClone = document.importNode(navElement, true);
+  parent.insertBefore(navClone, placeHolder);
+  parent.removeChild(placeHolder);
+}
